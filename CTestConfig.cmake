36e80bad343edf3518352e24fc8d08fb2f8ad5a5
Saad <saadmuizz@gmail.com>
2023-11-20 05:40:07 -0500

    update

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,7 +9,6 @@
 set(CTEST_PROJECT_NAME nanoJSONc)
 set(CTEST_NIGHTLY_START_TIME 01:00:00 UTC)
 
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=nanoJSONc")
+set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=nanoJSONc)
 
 set(CTEST_DROP_SITE_CDASH TRUE)
