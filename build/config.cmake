fbcdd911179d95f1225916f7374cce722ed1c9f1
Saad <saadmuizz@gmail.com>
2023-12-31 08:39:46 -0500

    update

--- build/config.cmake
+++ build/config.cmake
@@ -2,6 +2,7 @@
 #include(${CMAKE_CURRENT_LIST_DIR}/nanojsonc-targets.cmake)
 #check_required_components(nanojsonc)
 
+include(GNUInstallDirs)
 include(CMakeFindDependencyMacro)
 
 # Declare the dependency on nanojsonc
@@ -12,6 +13,6 @@ add_library(nanojsonc::nanojsonc INTERFACE IMPORTED)
 
 # Set target properties
 set_target_properties(nanojsonc::nanojsonc PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${CURRENT_PACKAGES_DIR}/include"
-        INTERFACE_LINK_LIBRARIES "${CURRENT_PACKAGES_DIR}/lib/libnanojsonc.a"
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_INCLUDEDIR}/nanojsonc"
+        INTERFACE_LINK_LIBRARIES "${CMAKE_INSTALL_LIBDIR}/libnanojsonc.a"
 )
