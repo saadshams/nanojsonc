32f9725c3c4391c92d91db763e4cf8b5bcdc6574
Saad <saadmuizz@gmail.com>
2023-12-31 14:51:50 -0500

    update

--- build/config.cmake
+++ build/config.cmake
@@ -1,18 +1,19 @@
-#@PACKAGE_INIT@
-#include(${CMAKE_CURRENT_LIST_DIR}/nanojsonc-targets.cmake)
-#check_required_components(nanojsonc)
+@PACKAGE_INIT@
 
-include(GNUInstallDirs)
-include(CMakeFindDependencyMacro)
+include(${CMAKE_CURRENT_LIST_DIR}/nanojsonc-targets.cmake)
+check_required_components(nanojsonc)
 
-# Declare the dependency on nanojsonc
-find_dependency(nanojsonc REQUIRED)
-
-# Create imported target
-add_library(nanojsonc::nanojsonc INTERFACE IMPORTED)
-
-# Set target properties
-set_target_properties(nanojsonc::nanojsonc PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_INCLUDEDIR}/nanojsonc"
-        INTERFACE_LINK_LIBRARIES "${CMAKE_INSTALL_LIBDIR}/libnanojsonc.a"
-)
+#include(GNUInstallDirs)
+#include(CMakeFindDependencyMacro)
+#
+## Declare the dependency on nanojsonc
+#find_dependency(nanojsonc REQUIRED)
+#
+## Create imported target
+#add_library(nanojsonc::nanojsonc INTERFACE IMPORTED)
+#
+## Set target properties
+#set_target_properties(nanojsonc::nanojsonc PROPERTIES
+#        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_INCLUDEDIR}/nanojsonc"
+#        INTERFACE_LINK_LIBRARIES "${CMAKE_INSTALL_LIBDIR}/libnanojsonc.a"
+#)
